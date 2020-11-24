f5aab6954f40032cb3a97d3ef20a40f2dcd49990
Khanh Loi Nguyen <nkloi@hcmut.edu.vn>
2020-11-24 22:49:09 +0700

    first commit

---
+++ InsitePolygon/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace InsitePolygon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbSite = new System.Windows.Forms.Label();
+            this.txtListSite = new System.Windows.Forms.TextBox();
+            this.btBrowserSite = new System.Windows.Forms.Button();
+            this.lbPolygon = new System.Windows.Forms.Label();
+            this.txtListPolygon = new System.Windows.Forms.TextBox();
+            this.btBrowserPolygon = new System.Windows.Forms.Button();
+            this.btRun = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbSite
+            // 
+            this.lbSite.AutoSize = true;
+            this.lbSite.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbSite.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSite.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.lbSite.Location = new System.Drawing.Point(36, 34);
+            this.lbSite.Name = "lbSite";
+            this.lbSite.Size = new System.Drawing.Size(66, 13);
+            this.lbSite.TabIndex = 0;
+            this.lbSite.Text = "List of site";
+            this.toolTip1.SetToolTip(this.lbSite, "Click to export form");
+            this.lbSite.Click += new System.EventHandler(this.lbSite_Click);
+            // 
+            // txtListSite
+            // 
+            this.txtListSite.Location = new System.Drawing.Point(108, 31);
+            this.txtListSite.Name = "txtListSite";
+            this.txtListSite.Size = new System.Drawing.Size(184, 20);
+            this.txtListSite.TabIndex = 1;
+            // 
+            // btBrowserSite
+            // 
+            this.btBrowserSite.Location = new System.Drawing.Point(298, 29);
+            this.btBrowserSite.Name = "btBrowserSite";
+            this.btBrowserSite.Size = new System.Drawing.Size(75, 23);
+            this.btBrowserSite.TabIndex = 2;
+            this.btBrowserSite.Text = "Browser";
+            this.btBrowserSite.UseVisualStyleBackColor = true;
+            this.btBrowserSite.Click += new System.EventHandler(this.btBrowserSite_Click);
+            // 
+            // lbPolygon
+            // 
+            this.lbPolygon.AutoSize = true;
+            this.lbPolygon.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPolygon.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.lbPolygon.Location = new System.Drawing.Point(12, 61);
+            this.lbPolygon.Name = "lbPolygon";
+            this.lbPolygon.Size = new System.Drawing.Size(90, 13);
+            this.lbPolygon.TabIndex = 0;
+            this.lbPolygon.Text = "List of polygon";
+            this.toolTip1.SetToolTip(this.lbPolygon, "Click to export form");
+            this.lbPolygon.Click += new System.EventHandler(this.lbPolygon_Click);
+            // 
+            // txtListPolygon
+            // 
+            this.txtListPolygon.Location = new System.Drawing.Point(108, 58);
+            this.txtListPolygon.Name = "txtListPolygon";
+            this.txtListPolygon.Size = new System.Drawing.Size(184, 20);
+            this.txtListPolygon.TabIndex = 1;
+            // 
+            // btBrowserPolygon
+            // 
+            this.btBrowserPolygon.Location = new System.Drawing.Point(298, 56);
+            this.btBrowserPolygon.Name = "btBrowserPolygon";
+            this.btBrowserPolygon.Size = new System.Drawing.Size(75, 23);
+            this.btBrowserPolygon.TabIndex = 2;
+            this.btBrowserPolygon.Text = "Browser";
+            this.btBrowserPolygon.UseVisualStyleBackColor = true;
+            this.btBrowserPolygon.Click += new System.EventHandler(this.btBrowserPolygon_Click);
+            // 
+            // btRun
+            // 
+            this.btRun.Location = new System.Drawing.Point(171, 84);
+            this.btRun.Name = "btRun";
+            this.btRun.Size = new System.Drawing.Size(75, 23);
+            this.btRun.TabIndex = 2;
+            this.btRun.Text = "Run";
+            this.btRun.UseVisualStyleBackColor = true;
+            this.btRun.Click += new System.EventHandler(this.btRun_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(331, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Naebolo@2020";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 155);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btRun);
+            this.Controls.Add(this.btBrowserPolygon);
+            this.Controls.Add(this.btBrowserSite);
+            this.Controls.Add(this.txtListPolygon);
+            this.Controls.Add(this.lbPolygon);
+            this.Controls.Add(this.txtListSite);
+            this.Controls.Add(this.lbSite);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Get site in polygon";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbSite;
+        private System.Windows.Forms.TextBox txtListSite;
+        private System.Windows.Forms.Button btBrowserSite;
+        private System.Windows.Forms.Label lbPolygon;
+        private System.Windows.Forms.TextBox txtListPolygon;
+        private System.Windows.Forms.Button btBrowserPolygon;
+        private System.Windows.Forms.Button btRun;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ InsitePolygon/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InsitePolygon
+{
+    public partial class Form1 : Form
+    {
+        public string currentDirect = Directory.GetCurrentDirectory();
+        OpenFileDialog fdlg = new OpenFileDialog();
+
+        public class Site
+        {
+            public string name;
+            public locationPoint location;
+
+        }
+
+        public class Polygon
+        {
+            public string name;
+            public List<locationPoint> polygon;
+        }
+
+        public class locationPoint
+        {
+            public double X, Y;
+        }
+
+
+        List<Site> lsSite = new List<Site>();
+        List<Polygon> lsPolygon = new List<Polygon>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lbSite_Click(object sender, EventArgs e)
+        {
+            string outputFile;
+            int i = 0;
+            outputFile = currentDirect + "\\" + "Form_List_Site" + "_" + DateTime.Now.ToString("yyyyMMddHHmmss") + @".txt";
+            StreamWriter swOut = new StreamWriter(outputFile);
+
+            swOut.WriteLine("Site Name\tLong\tLat");
+            swOut.WriteLine("HCM0001\t104.234252\t10.3434");
+            swOut.WriteLine("HCM0002\t104.234252\t11.3434");
+            swOut.Close();
+            MessageBox.Show("Export from to: " + outputFile);
+        }
+
+        private void lbPolygon_Click(object sender, EventArgs e)
+        {
+            string outputFile;
+            int i = 0;
+            outputFile = currentDirect + "\\" + "Form_List_Polygon" + "_" + DateTime.Now.ToString("yyyyMMddHHmmss") + @".txt";
+            StreamWriter swOut = new StreamWriter(outputFile);
+
+            swOut.WriteLine("Polygon Name\tLong\tLat");
+            swOut.WriteLine("Polygon01\t104.234252\t10.3434");
+            swOut.WriteLine("Polygon01\t104.34324\t11.6546");
+            swOut.WriteLine("Polygon01\t104.6767\t10.45645");
+            swOut.WriteLine("Polygon01\t104.3424\t11.7675");
+
+            swOut.WriteLine("Polygon02\t104.768\t10.987");
+            swOut.WriteLine("Polygon02\t104.67868\t11.89");
+            swOut.WriteLine("Polygon02\t104.345\t10.789");
+            swOut.WriteLine("Polygon02\t104.345\t11.789");
+            swOut.Close();
+            MessageBox.Show("Export from to: " + outputFile);
+        }
+
+        private void btBrowserSite_Click(object sender, EventArgs e)
+        {
+            if (currentDirect == "") currentDirect = Directory.GetCurrentDirectory();
+            fdlg.Multiselect = false;
+            fdlg.Title = "Open List of Site";
+            fdlg.InitialDirectory = currentDirect;
+            fdlg.Filter = "txt (*.txt)|*.txt";
+            fdlg.FilterIndex = 1;
+            fdlg.RestoreDirectory = true;
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (StreamReader sReader = new StreamReader(File.OpenRead(fdlg.FileName)))
+                    {
+                        string header = sReader.ReadLine();
+                        while (!sReader.EndOfStream)
+                        {
+                            string sFileRead = sReader.ReadLine();
+                            string[] oneLine = sFileRead.Split("\t,;".ToCharArray());
+                            locationPoint tmpPoint = new locationPoint();
+                            Site tmpSite = new Site();
+                            tmpPoint.X = Convert.ToDouble(oneLine[1]);
+                            tmpPoint.Y = Convert.ToDouble(oneLine[2]);
+                            tmpSite.name = oneLine[0];
+                            tmpSite.location = tmpPoint;
+                            lsSite.Add(tmpSite);
+                        }
+                    }
+                    txtListSite.Text = fdlg.FileName;
+                    MessageBox.Show("Import list site ok");
+                }
+                catch {
+                    MessageBox.Show("Can't import list site");
+                }
+            }
+        }
+
+
+
+        private void btBrowserPolygon_Click(object sender, EventArgs e)
+        {
+            if (currentDirect == "") currentDirect = Directory.GetCurrentDirectory();
+            fdlg.Multiselect = false;
+            fdlg.Title = "Open List of Site";
+            fdlg.InitialDirectory = currentDirect;
+            fdlg.Filter = "txt (*.txt)|*.txt";
+            fdlg.FilterIndex = 1;
+            fdlg.RestoreDirectory = true;
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (StreamReader sReader = new StreamReader(File.OpenRead(fdlg.FileName)))
+                    {
+                        string header = sReader.ReadLine();
+                        string old = "-";
+                        Polygon tmpPolygon = new Polygon();
+                        tmpPolygon.polygon = new List<locationPoint>();
+                        while (!sReader.EndOfStream)
+                        {
+                            string sFileRead = sReader.ReadLine();
+                            string[] oneLine = sFileRead.Split("\t,;".ToCharArray());
+                            if ((old != oneLine[0]) && (old != "-")) {
+                                tmpPolygon.name = old;
+                                lsPolygon.Add(tmpPolygon);
+                                tmpPolygon = new Polygon();
+                                tmpPolygon.polygon = new List<locationPoint>();
+                            }
+                            locationPoint tmpLocation = new locationPoint();
+                            tmpLocation.X = Convert.ToDouble(oneLine[1]);
+                            tmpLocation.Y = Convert.ToDouble(oneLine[2]);
+                            tmpPolygon.polygon.Add(tmpLocation);
+                            old = oneLine[0];
+                        }
+                        tmpPolygon.name = old;
+                        lsPolygon.Add(tmpPolygon);
+                    }
+                    txtListPolygon.Text = fdlg.FileName;
+                    MessageBox.Show("Import list polygon ok");
+                }
+                catch
+                {
+                    MessageBox.Show("Can't import list polygon");
+                }
+            }
+        }
+
+
+
+        private void btRun_Click(object sender, EventArgs e)
+        {
+
+            string outputFile = currentDirect + "\\" + "Result" + "_" + DateTime.Now.ToString("yyyyMMddHHmmss") + @".txt";
+            StreamWriter swOut = new StreamWriter(outputFile);
+
+            swOut.WriteLine("Site\tLong\tLat\tPolygon");
+            
+            for (int i = 0; i < lsSite.Count; i++)
+            {
+                for (int j = 0; j < lsPolygon.Count; j++)
+                {
+                    if (checkInsitePolygon(lsSite[i].location, lsPolygon[j].polygon))
+                    {
+                        swOut.WriteLine(lsSite[i].name + "\t" + lsSite[i].location.X.ToString() + "\t" + lsSite[i].location.Y.ToString() + "\t" + lsPolygon[j].name);
+                        break;
+                    }
+                }
+            }
+
+            swOut.Close();
+            MessageBox.Show("Export from to: " + outputFile);
+        }
+
+        public static bool checkInsitePolygon(locationPoint A, List<locationPoint> input_polygon)
+        {
+            /*-----------------------------------------------------
+            Xác định 1 điểm có nằm trong polygon hay không
+            -----------------------------------------------------*/
+            locationPoint P11 = new locationPoint();
+            locationPoint intersectPoint;
+            locationPoint Zero = new locationPoint();
+            int numIntersect = 0;
+            bool checkInter = false;
+            Zero.X = 0;
+            Zero.Y = 0;
+            P11 = input_polygon[0];
+            for (int j = 1; j < input_polygon.Count; j++)
+            {
+                if (P11.X != input_polygon[j].X || P11.Y != input_polygon[j].Y)
+                {
+                    intersectPoint = new locationPoint();
+                    checkInter = Intersect2(P11, input_polygon[j], Zero, A, out intersectPoint);
+                    if (checkInter) numIntersect++;
+                }
+                P11 = input_polygon[j];
+            }
+            intersectPoint = new locationPoint();
+            checkInter = Intersect2(P11, input_polygon[0], Zero, A, out intersectPoint);
+            if (checkInter) numIntersect++;
+            return numIntersect % 2 != 0;
+        }
+
+
+        public static bool Intersect2(locationPoint P11, locationPoint P12, locationPoint P21, locationPoint P22, out locationPoint IntersectP)
+        {
+            /*-----------------------------------------------------
+            Xác định 2 đường thẳng giao nhau
+            -----------------------------------------------------*/
+            double s02_x, s02_y, s10_x, s10_y, s32_x, s32_y, s_numer, t_numer, denom, t;
+            s10_x = P12.X - P11.X;
+            s10_y = P12.Y - P11.Y;
+            s32_x = P22.X - P21.X;
+            s32_y = P22.Y - P21.Y;
+            IntersectP = new locationPoint();
+            IntersectP.X = -1;
+            IntersectP.Y = -1;
+            denom = s10_x * s32_y - s32_x * s10_y;
+            if (denom == 0)
+            {
+                //if (Is_In(P11.X,P21.X,P22.X) || Is_In(P12.X,P21.X,P22.X)) return true;
+                return false; // Collinear
+            }
+            bool denomPositive = denom > 0;
+
+            s02_x = P11.X - P21.X;
+            s02_y = P11.Y - P21.Y;
+            s_numer = s10_x * s02_y - s10_y * s02_x;
+            if ((s_numer < 0) == denomPositive)
+                return false; // No collision
+
+            t_numer = s32_x * s02_y - s32_y * s02_x;
+            if ((t_numer < 0) == denomPositive)
+                return false; // No collision
+
+            if (((s_numer > denom) == denomPositive) || ((t_numer > denom) == denomPositive))
+                return false; // No collision
+            // Collision detected
+            t = t_numer / denom;
+            IntersectP.X = P11.X + (t * s10_x);
+            IntersectP.Y = P11.Y + (t * s10_y);
+
+            return true;
+        }
+
+        
+    }
+}
